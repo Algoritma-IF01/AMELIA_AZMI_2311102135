9c0645c3994d5c3cf688f80a6288c74ab79843b9
Amelia Azmi <162032965+ameliaesmoe@users.noreply.github.com>
2024-10-06 19:46:25 +0700

    praktikum2

--- Praktikum2/Latihan4.go
+++ Praktikum2/Latihan4.go
@@ -7,14 +7,14 @@ import (
 func main() {
 	var celcius, fahrenheit, kelvin, reamur float64
 
-	fmt.Print("Masukkan suhu dalam celcius: ")
+	fmt.Print("Temperatur celcius: ")
 	fmt.Scanln(&celcius)
 	reamur = celcius * 4 / 5
 	fahrenheit = (celcius * 9 / 5) + 32
 	kelvin = celcius + 273.15
 
-	fmt.Println("Suhu dalam reamur: ", reamur)
-	fmt.Println("Suhu dalam fahrenheit: ", fahrenheit)
-	fmt.Println("Suhu dalam kelvin: ", kelvin)
+	fmt.Println("Derajat reamur: ", reamur)
+	fmt.Println("Derajat fahrenheit: ", fahrenheit)
+	fmt.Println("Derajat kelvin: ", kelvin)
 
 }
